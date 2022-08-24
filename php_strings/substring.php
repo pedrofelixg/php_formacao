df9fd0b4ec8020e8c2041548eea2b4326958d6d6
pedro.viniciusacm@gmail.com <pedro.viniciusacm@gmail.com>
2022-08-23 22:31:55 -0300

    pequenas modificações

--- php_strings/substring.php
+++ php_strings/substring.php
@@ -1,9 +1,11 @@
 <?php
-$email = 'ana_julia_moura@alura.com.br';
+$email = 'melissa@alura.com.br';
 $posicaoArroba = strpos($email, "@"); //retorna um inteiro do parâmetro indicado na str avaliada.
 
 echo $posicaoArroba . PHP_EOL;
 
-echo substr($email, 0, $posicaoArroba) . PHP_EOL;
+$usuario = substr($email, 0, $posicaoArroba);
+
+echo strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicaoArroba +1) . PHP_EOL;
 // como no exemplo acima, a emição do último parâmetro não quebra a função.
