5c52c2d1a9f8e51cbe885661e3807f24243d956d
pedrofelixg <pedro,viniciusacm@gmail.com>
2023-02-03 20:05:28 -0300

    alterações bobas

--- php_funcoes/gui_teste.php
+++ php_funcoes/gui_teste.php
@@ -5,9 +5,14 @@ $floats = [
     [8.0, 9.0, 10.0]
 ];
 // busca por tentar usar esse conjunto de arrays para a função requerida
-$loops = 3;
 function calcMedia ($float1, $float2, $float3) {
-        echo ($float1 + $float2 + $float3) / 3;
+    $peso1 = 2;
+    $peso2 = 3;
+    $peso3 = 5;
+
+    $pesos = $peso1 + $peso2 + $peso3;
+
+    echo round(((($float1 * $peso1) + ($float2 * $peso2) + ($float3 * $peso3)) / $pesos), 1);
 }
 
-calcMedia(6.5, 4.3, 6.2);
+calcMedia(5.1, 4.2, 8.1);
