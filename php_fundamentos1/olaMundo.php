8eabfd9abce50de3e198e7ba3231adfc4fe6f012
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-03-15 09:54:54 -0300

    primeiros arquivos das aulas de fundamentos em php

---
+++ php_fundamentos1/olaMundo.php
@@ -0,0 +1,8 @@
+<?php
+$abertura = "Olá Mundo";
+
+echo $abertura . PHP_EOL;
+
+/*
+estrutura básica: declaração de variável e impressão dela no terminal com o comand echo
+*/
---
+++ php_fundamentos1/tiposDados.php
@@ -0,0 +1,17 @@
+<?php
+// quais tipos de dados suportados pelo php? todos que uma Linguagem de Programação suporta
+
+$inteiros = 25;
+$floats = 121.5;
+$caracteres = "Pedro";
+$booleanos = true;
+    $booleanoAlt = false;
+$arrays = [1, 40, 25, 32];
+
+echo $inteiros . PHP_EOL;
+echo "olá meu nome é $caracteres e tenho $inteiros anos" . PHP_EOL;
+
+//retornos:
+//25
+//olá meu nome é Pedro e tenho 25 anos
+
